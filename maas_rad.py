211a6c3d15b24fc2238c8f3cc07ffdaafa8d9a87
bill <bill.wear@canonical.com>
2023-01-13 15:11:49 -0600

    retroactive commit of v1.0 beta 2

---
+++ maas_rad.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+# what do i need this program to do?
+## how do i process selectors?
+## how do i multiplex links?
+## how do i build a switcher?
+## how do i manage targets?
+## how do i publish older versions?
+## how do i publish current versions?
+## how do i publish to html?
+## how do i publish to other formats?
+## find changes
+## notify the editor of the changes
+## retrieve the changes for the editor
+## help the editor spot the differences
+
+def process_selectors():
+    # where do i look for selectors?
+    # how do i recognize selectors?
+    # what do i do with the selectors?
+    pass
+
+def multiplex_links():
+    # where do i look for links?
+    # how do i recognize links?
+    # what do i do with the links?
+    pass
+
+def build_a_switcher():
+    # where do i build a switcher?
+    # what does a switcher look like?
+    
+    pass
+
+def manage_targets():
+    pass
+
+def publish_older_versions():
+    pass
+
+def publish_current_versions():
+    pass
+
+def publish_html():
+    pass
+
+def publish_other():
+    pass
+
+def find_changes():
+    pass
+
+def notify_editor():
+    pass
+
+def retreive_changes():
+    pass
+
+def highlight_diffs():
+    pass
