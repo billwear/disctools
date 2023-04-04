861ebe4790222389fe0f3dce8907b7bf850b8df7
bill <bill.wear@canonical.com>
2023-04-04 17:20:11 -0500

    updating toolset to latest

---
+++ lintredir.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+# read maas-documentation-25.md into memory
+# scan URL list for multiple redirects
+# how to tell which redirect is the final one?
+## the final one is in the URL list
